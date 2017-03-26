cc94483b2ab145aec570a6dfa152c5db4f1c7dd8
tajson <laski.tajka@gmail.com>
2017-03-26 19:37:20 +0200

    add js comments

---
+++ scripts.js
@@ -0,0 +1,5 @@
+var age = prompt('Ile masz lat?');
+alert('Woohoo, you survived ' + age + ' years on planet Earth! Partyy!');
+
+//nie wiem, czy dokładnie o to chodziło w tym drugim zadaniu
+//ale chyba tak!
